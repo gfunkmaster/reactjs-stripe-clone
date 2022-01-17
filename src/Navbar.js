d3896ae7e2adfa574632270e0350686d9a4cdfc3
gfunkmaster <nurkicelmin@hotmail.com>
2022-01-17 20:14:12 +0100

    Creaateing navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import logo from './images/logo.svg'
 import { FaBars } from 'react-icons/fa'
-
+import { useGlobalContext } from './context'
 const Navbar = () => {
+  const {} = useGlobalContext();
   return <h2>navbar component</h2>
 }
 
